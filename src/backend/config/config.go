5ea78c517b886f91d085fcc1d52c05eb3846d54d
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-04-25 12:09:13 +0200

    Adding logging to config package and clean up code a bit

--- src/backend/config/config.go
+++ src/backend/config/config.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71ec6dad09643d2d118f08f565577fe2a027b7a52a47b4ba7e9e258a5523ba4c
-size 495
+oid sha256:87540e7559cffbe703835fd5eb7e224fe68a214c07c4da2a2c406ac59e47a2d6
+size 550
---
+++ src/backend/config/models.go
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:038ca3c4fe77ceb0b7dd13da2b7797bfbd088c7ceed06973d4c40ef3bfedd4d0
+size 79
