bbc7169f24c19129ebdf2630eee47cd3b498bed1
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-04-25 14:17:00 +0200

    Better JWT error handling and do not expose errors to client

--- src/backend/utils/jwt.go
+++ src/backend/utils/jwt.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f883a9428c8e2b0e5ecd5f3eb9b178250c6e0c52625c47b6ee0483974340076
-size 1515
+oid sha256:1682c4518ffadaff7d54a4faabba3c86a00262d0028f2d8ecf75fb7c6475ba88
+size 1814
