bae370ce76e959eafd895d1ba7ab25a2b4ce7e43
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-04-25 13:31:13 +0200

    Adding another JWT helper function and streamline trading code

--- src/backend/microservices/tradingService/app.go
+++ src/backend/microservices/tradingService/app.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:151ccbc56b06ba5c5bf3d486248ff3a53e49dd23693ae2d13768a1428048c64d
-size 1282
+oid sha256:cde3051e857ebd0c2a2fac6d5b492ea72700300204985ef2b8a2c4bd33bb4ff9
+size 239
--- src/backend/microservices/tradingService/handlers.go
+++ src/backend/microservices/tradingService/handlers.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcbaf48d6997202d4bee072994b828caeb47c22bb147c6d5e5f8ac8fc0ba7f29
-size 3341
+oid sha256:dd7a8e6171db5ba778c78b5e6ad567671fc15fefe46b9f20fb37bddccd60ae7e
+size 3415
