8e0b664ff48a2eaccf389141370ab15441e78c76
Nico <nico24sg@gmail.com>
2021-04-26 16:37:23 +0200

    Fix: Display of entries in database

--- src/backend/microservices/userService/handlers.go
+++ src/backend/microservices/userService/handlers.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f73aa770184cb2d609f2df6f25b5f350694c8154780782aa5a003892b2097c75
-size 2401
+oid sha256:c8f8564135a4d17ca4afaaa9e0b7ec5373190863f14a3239a98a3aadc33c447f
+size 2423
