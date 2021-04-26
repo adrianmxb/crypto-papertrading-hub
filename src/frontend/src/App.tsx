0bb99054e8c20033062a74a23cfdd03d5a10a5d7
Adrian Busse <git@adrianmxb.com>
2021-04-26 11:50:26 +0200

    add toasts to inform user when performing an action

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:194fd1c80fca9e1607055cfd76dbce4038705ac0320ae4da4258e8550e287438
-size 963
+oid sha256:ed53185e5a1ff2b8ae3b99e85a4d205a7fc32b0e5ed473971d5fe18232e04a35
+size 1122
--- src/frontend/src/auth/pages/Login.tsx
+++ src/frontend/src/auth/pages/Login.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6de39270c885cebe13f7b00b567940e6144487929dc8305ac8094477c991c6c
-size 3077
+oid sha256:761baf22fab1f0eacc69f8418099e04876dff5d66a8df380b83964ebb894c07b
+size 3413
