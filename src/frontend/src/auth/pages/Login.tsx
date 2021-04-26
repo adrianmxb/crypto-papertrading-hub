b2679b27cc69c8173962024bfa8d68877a8dd47f
Adrian Busse <git@adrianmxb.com>
2021-04-26 09:50:32 +0200

    add helpers for authentication

--- src/frontend/src/auth/pages/Login.tsx
+++ src/frontend/src/auth/pages/Login.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf56dbf4ec4154d5225b3d354c85c167170705aeb408ac17f8564fb1d3b6c05f
-size 2431
+oid sha256:ffe1cff3ba1be55e6ad12a1b89d6c60f34d2770e6f7e6ddef3723afa3ab33bad
+size 2615
