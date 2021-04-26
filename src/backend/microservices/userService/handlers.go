d606472dea4e28657bea07936cbe765db49d4695
Nico <nico24sg@gmail.com>
2021-04-26 13:39:27 +0200

    Fix: Password wurde nicht serialisiert bei der Nutzererstellung

--- src/backend/microservices/userService/handlers.go
+++ src/backend/microservices/userService/handlers.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e2f2eb1f32d62c3a6c78a759cccd2afd138005d5eef4369a759a3da043596af
-size 2638
+oid sha256:c853f326bcb656a40f0e53c1c1aa1ec66bcdd6f8532db4b2be75f098889bd2cd
+size 2913
--- src/backend/models/User.go
+++ src/backend/models/User.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76d22072abd1fbed9fbda4cfaae0798c9b01040ce451d87bc303beb1911e751f
-size 239
+oid sha256:fb5915a1aa4333da0d7268acfcbe4dd7b6f0a3bf0dda07dc7432429e3df01293
+size 303
