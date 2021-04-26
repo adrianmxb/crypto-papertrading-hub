52acfdb3eba8aab575632fb3ac46ca1907c114b8
Adrian Busse <git@adrianmxb.com>
2021-04-26 08:18:10 +0200

    add sign up button to login page

--- src/frontend/src/auth/pages/Login.tsx
+++ src/frontend/src/auth/pages/Login.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:825cfc0c1bc10a384c8e0ac5b912a2666beae51a379e5d6710c69d0a6b0203dc
-size 2334
+oid sha256:cf56dbf4ec4154d5225b3d354c85c167170705aeb408ac17f8564fb1d3b6c05f
+size 2431
