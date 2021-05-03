6b2b206d1ef0edcc1565ce35806efcdea3a9ccac
NicoSe <nico24sg@gmail.com>
2021-05-03 09:28:39 +0200

    Refactor table primary key type and add fake data script

--- src/backend/microservices/userService/handlers.go
+++ src/backend/microservices/userService/handlers.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8f8564135a4d17ca4afaaa9e0b7ec5373190863f14a3239a98a3aadc33c447f
-size 2423
+oid sha256:bc38a9ee2d1f73d09420517acb736348549cb8b8f460622d02e9ad396260aaf5
+size 2371
