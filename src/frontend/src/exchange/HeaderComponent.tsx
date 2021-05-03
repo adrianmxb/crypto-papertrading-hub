21b635a427f092c454c88855794283d214ba03b4
Adrian Busse <git@adrianmxb.com>
2021-05-03 08:58:36 +0200

    fix project name on header component

--- src/frontend/src/exchange/HeaderComponent.tsx
+++ src/frontend/src/exchange/HeaderComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:613e8cc8f072e2adc9241dec7a88d83a3275752e8c4d5b56c38c7e3cbc034bb3
-size 3227
+oid sha256:081ce0a709c03b9a98e774137f63a23dbf9674e2531fec8cbb02af62ebc04f3f
+size 3217
