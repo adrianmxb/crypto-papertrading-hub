27c5d3faff3df938a47d0b5ea96548cd8d6b0984
Adrian Busse <git@adrianmxb.com>
2021-05-03 08:08:16 +0200

    add temporary registration submit to login page

--- src/frontend/src/auth/pages/Login.tsx
+++ src/frontend/src/auth/pages/Login.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:761baf22fab1f0eacc69f8418099e04876dff5d66a8df380b83964ebb894c07b
-size 3413
+oid sha256:02495303ff877d62d4d7873beb4a1e47f89bf609cc8c76f3aa6d4ec9cef6db47
+size 4237
