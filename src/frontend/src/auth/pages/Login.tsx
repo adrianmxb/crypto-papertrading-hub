fe1bb07804b34e109df8c68785c9d5b3bd34a521
Adrian Busse <git@adrianmxb.com>
2021-05-03 08:57:53 +0200

    add better error handling for login requests

--- src/frontend/src/auth/pages/Login.tsx
+++ src/frontend/src/auth/pages/Login.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02495303ff877d62d4d7873beb4a1e47f89bf609cc8c76f3aa6d4ec9cef6db47
-size 4237
+oid sha256:dc1835c6555fb8c5df6ace0afa98791b790df5fad54fdb89ff07e07282ba52dc
+size 4330
