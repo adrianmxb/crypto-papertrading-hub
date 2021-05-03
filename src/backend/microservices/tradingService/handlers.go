bc36ad7be5f3c18b17646851a6e84bea4b022536
NicoSe <nico24sg@gmail.com>
2021-05-03 08:14:27 +0200

    add todo

--- src/backend/microservices/tradingService/handlers.go
+++ src/backend/microservices/tradingService/handlers.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:207157e6cb3e9e0ddcb8bfb2f846d922f65ee99a2be9009c2fadd2926fc693c6
-size 4546
+oid sha256:52d9487d5cdad2553b2232aac4c1565e0cb4d0f6dd366802804da547f24a2ea8
+size 4589
