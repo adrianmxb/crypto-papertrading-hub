4878ae1076d9103c3802c84f7f1f91d78f974213
NicoSe <nico24sg@gmail.com>
2021-05-10 14:30:06 +0200

    refactoring

--- src/backend/cmd/testClient/client.go
+++ src/backend/cmd/testClient/client.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb7a77f9ac75282b56de0f94e7de230836efda54705d4a5609b6f5607adbe20b
-size 668
+oid sha256:4c632a2b9e5086b7eedba9ecae5cbae2770a2877e1d6f96650f1aaa642d36641
+size 674
---
+++ src/backend/microservices/marketData/webSocketHandler.go
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:004e85bee9dc2a906674aadf24e18e15554c5ac31eadfa2f4c64fcf9811aa59a
+size 2227
