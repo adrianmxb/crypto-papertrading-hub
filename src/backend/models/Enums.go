62ed4dc6f5d54d2705b35d94d9ea217e64231eac
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-05-15 22:01:57 +0200

    Removing unused currency variables

--- src/backend/models/Enums.go
+++ src/backend/models/Enums.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b275e3f72f1de2ca2cea049f96b0b35793e50e1934ea548dd7bf27a92c167408
-size 491
+oid sha256:4568a94693691d32d8d0b5cc82af3a44f45120c0055751f25c82cfcbc1d8611e
+size 372
