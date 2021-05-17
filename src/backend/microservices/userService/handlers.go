d22f36271e1a5626506f05cf6351ff13b92c22a1
Adrian Busse <git@adrianmxb.com>
2021-05-17 09:35:22 +0200

    fix check if user already exists on sign up

--- src/backend/microservices/userService/handlers.go
+++ src/backend/microservices/userService/handlers.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c67eff94388e2636ecc40eb3409299603f801f5b27f79f0f46d8a2734ee9225
+oid sha256:3812c8fee1dd7b8a1c99090eb0445fafd74a1e5b6a65ed9dac1288ca22c01ffa
 size 2656
