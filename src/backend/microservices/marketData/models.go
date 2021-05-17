1c0ac3cd175aaf78c33b766921fe40f953cc7932
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-05-17 10:33:54 +0200

    Refactoring UpdateClients() functions a bit

--- src/backend/microservices/marketData/models.go
+++ src/backend/microservices/marketData/models.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce27da1803f92511553622d2aa4ef76eb02dd039240f25bacee6809e4431de89
-size 474
+oid sha256:c787bea3f6b3e019499f8c416d6f2ba4f2dcb9fe9225e25c147634d188fe0d8f
+size 575
