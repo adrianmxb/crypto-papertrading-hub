8a2c1f829dc2feaed48304ca3807dba4e4a9c760
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-05-28 11:50:51 +0200

    Fixing code for initial portfolio setup duriong user creation

--- src/backend/microservices/userService/handlers.go
+++ src/backend/microservices/userService/handlers.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c588f0d476e7b21c20e9f8c7d6f39fe607e07d3408ec8f47024fff8ba7960c83
-size 2668
+oid sha256:14fddcf12aeddf79e9a55a7a38be3e627ab49088c074066ce799878f9038f3d5
+size 2702
