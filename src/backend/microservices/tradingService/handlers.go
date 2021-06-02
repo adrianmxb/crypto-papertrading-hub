b3998f73b313220a55e9e647bb280860f39b147b
Adrian Busse <adrianmxb@adrianmxb.com>
2021-06-02 09:17:14 +0000

    Merged in backend-fix-order-creation (pull request #21)
    Improve handling of websocket errors
    
    * improve handling of websocket errors
    
    * remove error forwarding to user facing system
    
    * use error method instead of fatal as we don't want to exit the process on error
    
    Approved-by: Thomas Schmidt
    Approved-by: Jannis Assenheimer

--- src/backend/microservices/tradingService/handlers.go
+++ src/backend/microservices/tradingService/handlers.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d369b4a2686ac2a443a49b960529e1c8dd6613254bea5fa534b67027367e0b0b
-size 6353
+oid sha256:11dac78f7029a0342b84e2583122c03fc15bf6fec193dca4fa472a5f93a0499a
+size 6374
--- src/backend/microservices/tradingService/utils.go
+++ src/backend/microservices/tradingService/utils.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15476e34ec7ca6d28fe7a17ea3f7337503ca758d6e755dcdae2a30ec67430fc1
-size 1463
+oid sha256:2c1798c1b6ca553eba52f2b404f5408632a82523375da3ecb51925b315e6b577
+size 1559
