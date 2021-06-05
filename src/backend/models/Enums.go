f07a0b28443a785fc419966b3fb0074febf39dc4
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-06-05 17:29:17 +0200

    Renaming some files

--- src/backend/models/Enums.go
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d52e3c03c601a1d692672652b485da4e8eef6b71966700b4da48f797ceedfa3b
-size 212
--- src/backend/models/Transaction.go
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8842c0b71dfb3402cdfbd02a78bb5ea00c5a1558f696710b2aebd43488eedc33
-size 288
