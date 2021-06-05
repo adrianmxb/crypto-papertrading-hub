a45ba5231ba9db9c439d15d4979b05b4f495c126
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-06-05 14:36:01 +0200

    Fetching assets from bitstamp, only EUR pairs

--- src/backend/microservices/market/handlers.go
+++ src/backend/microservices/market/handlers.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2028b2f6bcf5e06f0101a7ca997554114f668bad6a1a311b08e52e6e71ceb84
-size 2996
+oid sha256:48f7b05840139d2ff6f3a0d7b0ac3645c21908273aed122b2787adc0c99c1e66
+size 3121
--- src/backend/microservices/market/routes.go
+++ src/backend/microservices/market/routes.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3cceea69510070790b7579b3f0619f60b26104df99ff7ee73554a78b9c9b8ff
-size 262
+oid sha256:faa6b8cc7c2ec521ea27e6bb68d3a42306da8afee3d545f8c4e506e5ca5059f8
+size 306
