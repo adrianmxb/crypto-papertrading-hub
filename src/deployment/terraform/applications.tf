e21fdf65e9b9be5d597073b420fe348c96fe185b
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-06-06 10:44:53 +0200

    Switching to MySQL instead of Postgres :'D

---
+++ src/deployment/terraform/applications.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:98b4ab7bae5c6bab375fb1804575e18f18970765b7c08f5d91a9a8561113230c
+size 641
--- src/deployment/terraform/database.tf
+++ src/deployment/terraform/database.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0177d349a905e44019c1b4dc4079b9cc598d238b3133ada19ec3eae61396ad66
-size 734
+oid sha256:901727e409780fc2e9f35787e6b27f457dce36510338cfeb33f4f81a90d80ba5
+size 713
