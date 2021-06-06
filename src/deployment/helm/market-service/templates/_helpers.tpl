f36678196c0655eccc3fd1b57ce5993cda47862e
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-06-06 13:19:42 +0200

    Adding deployment for market service

---
+++ src/deployment/helm/market-service/templates/_helpers.tpl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0f304f3a40f22123a831d9500839f8b9c0255c018023818d92c07e53d65a757e
+size 919
---
+++ src/deployment/terraform/app_market-service.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b4e89c30bd0c7e42fc809a897d88ce9a9af011e5932c197836a229d1348f61de
+size 1284
