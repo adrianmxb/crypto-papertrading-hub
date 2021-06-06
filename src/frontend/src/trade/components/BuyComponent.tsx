657cbd0a48b30ce87dfb9249d3546419a5f62a64
Diimsch <57183037+Diimsch@users.noreply.github.com>
2021-06-06 23:13:14 +0200

    Support for all returned currencies

--- src/frontend/src/trade/components/BuyComponent.tsx
+++ src/frontend/src/trade/components/BuyComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f66b19acb24c655ec025909e2c108e5494d87fba7e8ce6431a469a1ca1860696
-size 2096
+oid sha256:b9529051e4fef1d7b0d32cb7896afe9ae15976058c0f9100a02d358c9b9a7ea2
+size 2187
--- src/frontend/src/trade/components/SellComponent.tsx
+++ src/frontend/src/trade/components/SellComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:129a899aba26b1ce11b1157795f776ea12f6264e5abefb8f1b1ffb659ad7ce9b
-size 2075
+oid sha256:95b6efc9bd4a74b183ac42238d608b7d6af409dc7c571dbcd1b5e2e847294db9
+size 2137
