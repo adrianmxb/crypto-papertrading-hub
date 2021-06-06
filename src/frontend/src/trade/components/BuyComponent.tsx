3ea1e37affdef35a1cfcfc4e0347ccd7768b6e40
Diimsch <57183037+Diimsch@users.noreply.github.com>
2021-06-06 23:24:59 +0200

    Fix transactions

--- src/frontend/src/trade/components/BuyComponent.tsx
+++ src/frontend/src/trade/components/BuyComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9529051e4fef1d7b0d32cb7896afe9ae15976058c0f9100a02d358c9b9a7ea2
-size 2187
+oid sha256:cd535c457bcf3506359da5f4ec7ac6a214ca422331ae8b5068b8a0910ab16a73
+size 2106
--- src/frontend/src/trade/components/SellComponent.tsx
+++ src/frontend/src/trade/components/SellComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95b6efc9bd4a74b183ac42238d608b7d6af409dc7c571dbcd1b5e2e847294db9
-size 2137
+oid sha256:43846d86a475e631c894905b2be016a33afff40bd7a1e7315bf08bf2130f7aed
+size 2123
