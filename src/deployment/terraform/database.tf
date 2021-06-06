0982e24b4a374695314347d438ab94c265be9e8c
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-06-06 14:12:02 +0200

    Setting CloudSQL flag require for /market/change query working

--- src/deployment/terraform/database.tf
+++ src/deployment/terraform/database.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:901727e409780fc2e9f35787e6b27f457dce36510338cfeb33f4f81a90d80ba5
-size 713
+oid sha256:45ed08b5e4b4365daaf6fb8f9cea7986769b8852be60e4ff8483b85513c138b0
+size 888
