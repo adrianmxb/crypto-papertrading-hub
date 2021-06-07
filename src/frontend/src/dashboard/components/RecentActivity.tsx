57445c6a11843e115ec63d0816a72f257786a9ba
Adrian Busse <git@adrianmxb.com>
2021-06-07 10:09:19 +0200

    fix column span in recent activity

--- src/frontend/src/dashboard/components/RecentActivity.tsx
+++ src/frontend/src/dashboard/components/RecentActivity.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba50893731a0d6e1a5d927c410d59605c2b5216dc10db6c46e3a400a8faaeda8
-size 2297
+oid sha256:d5fff8d7d1d77cbc1e3f3f1d63c5e6548e356ab49318591259ce258fa79365d0
+size 2324
