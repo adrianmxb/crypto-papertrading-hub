bdce539fa935cda4ff975a6e690180caeddfd2aa
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-06-07 11:48:09 +0200

    Fixing mongodb setup

--- src/deployment/terraform/app_mongodb.tf
+++ src/deployment/terraform/app_mongodb.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87a251f6c989997925f6db4e22e6b7a776cbabc8e64da9c582ecf7a3f224b945
-size 803
+oid sha256:5c981cd689e7442953702188a618092da45a1418361fe8aa84ea6717530e8445
+size 777
