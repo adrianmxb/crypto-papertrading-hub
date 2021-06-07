0d6a14d62ed8f175d48f0aab1a2e2508ff886be5
Adrian Busse <git@adrianmxb.com>
2021-06-07 09:58:25 +0200

    add type info for portfolio state

--- src/frontend/src/states.ts
+++ src/frontend/src/states.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9432319f7d85c5fa0161fd4403dcdc26a0bbfa447e79d3d01eea19c6f12b1312
-size 924
+oid sha256:7b1318bafe1713170d8de67d9bfef18776b6716858e31399545983aa30a51dd6
+size 933
