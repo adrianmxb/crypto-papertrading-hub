4dd1cc56dc6ac6b4dd4bca7d033d675ef8021824
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-06-07 11:48:57 +0200

    Adding deployment for news service

---
+++ src/deployment/helm/news-service/templates/_helpers.tpl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:91e56573141ee4110d4e004e4556f994a0d56491b9bcb5dc7932caf1633befdb
+size 905
---
+++ src/deployment/terraform/app_news-service.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:db6aab190933a1fd2c2d5ce7785cddb326d80c343aeac4be8ec05cce978b30d2
+size 1377
