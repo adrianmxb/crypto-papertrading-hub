f6fd0c6855678f8106afd5e9ebb8315bf7f3a9ec
Diimsch <57183037+Diimsch@users.noreply.github.com>
2021-06-07 09:50:52 +0200

    cleanup

--- src/frontend/src/trade/components/BuyComponent.tsx
+++ src/frontend/src/trade/components/BuyComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd535c457bcf3506359da5f4ec7ac6a214ca422331ae8b5068b8a0910ab16a73
-size 2106
+oid sha256:1412fe31e6183f94cf75c1b03382a8143d06fab1f49b78e8805c56f15a0de033
+size 1920
--- src/frontend/src/trade/components/SellComponent.tsx
+++ src/frontend/src/trade/components/SellComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43846d86a475e631c894905b2be016a33afff40bd7a1e7315bf08bf2130f7aed
-size 2123
+oid sha256:261c1fef45ce546cae7b999fda0124c1094282b21bdd61813029736e141c1928
+size 1966
