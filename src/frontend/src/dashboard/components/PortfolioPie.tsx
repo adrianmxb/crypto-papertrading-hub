cca4857ebef54cef3183933bb2a538619e797fed
Adrian Busse <git@adrianmxb.com>
2021-06-07 10:05:45 +0200

    fix data picking from color dictionary

--- src/frontend/src/dashboard/components/PortfolioPie.tsx
+++ src/frontend/src/dashboard/components/PortfolioPie.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f50b5605d4a38bd1672572eaf948adf67ce948a6ba5f2a25b00b1ea91b744eb3
-size 1609
+oid sha256:6e9e7594123a60f2a0fc166ee42633cf71f6d9032b2e5f31098aa55158ec64d8
+size 1570
