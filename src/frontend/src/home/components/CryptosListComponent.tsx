464ee2784031f173f6f2b7272213232723417603
Dimsch <57183037+Diimsch@users.noreply.github.com>
2021-06-12 17:40:19 +0200

    fix linechart

--- src/frontend/src/home/components/CryptosListComponent.tsx
+++ src/frontend/src/home/components/CryptosListComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b30b2d6f6c5484a5b6220635e65d56bb7f2cce2a5cc97f27f2cc7276627992b3
-size 3256
+oid sha256:5ebc4e9e3c34979094792188bb0f42fc021bef7fbea8196ed6325356f66b8c14
+size 3334
