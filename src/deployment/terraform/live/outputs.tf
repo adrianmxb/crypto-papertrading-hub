7aadc1eebef29dec995968bf10be161a9fed0ef6
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-06-13 23:19:02 +0200

    Adding MySQL IP as output

--- src/deployment/terraform/live/outputs.tf
+++ src/deployment/terraform/live/outputs.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19a68f3cd41cd7579c65c6b0d0dce802e49a26e52a61b8bec495eb8860c4b158
-size 254
+oid sha256:6781132cffe72768f31efde5ac466bc5858594dd7f6f38c5c9b648129e06b535
+size 337
