ceadf45c9b51a98226e7c8513996900a8f0c57af
Adrian Busse <git@adrianmxb.com>
2021-06-14 09:07:06 +0200

    improve error handling and remove unused components

--- src/frontend/src/trade/components/ConvertComponent.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:126d44f9f2b59d43d00d304752ad85344c8720ed74a96635c34f4168b73f4be3
-size 1105
--- src/frontend/src/trade/components/TransactionComponent.tsx
+++ src/frontend/src/trade/components/TransactionComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac5b5d4ad634231145a71f8a4c3112a50d983ff7a4d0e0cbd33e551e98ce3afa
-size 3791
+oid sha256:f1009ed66a53098d23f890e0577995e185b611a5e15b748e44bb9cbae794c60a
+size 3314
