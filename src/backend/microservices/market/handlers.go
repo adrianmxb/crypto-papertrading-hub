8d8e13a9737a43818138e6fa99907930910f2c0a
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-06-14 19:20:56 +0200

    Improve reliability of market data itself by providing fallback. Deleting market data older than 30h to improve performance of change endpoint. Remove spam log from service

--- src/backend/microservices/market/handlers.go
+++ src/backend/microservices/market/handlers.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd6d69db226dd8983c26278503703b27d3aac4edc7994dbb5dbae6b0fc9130be
-size 2850
+oid sha256:4b2e684164f3bbffd0f630d4e17e87678b68bb9703887a1c8fb3515ad2c021a4
+size 3628
