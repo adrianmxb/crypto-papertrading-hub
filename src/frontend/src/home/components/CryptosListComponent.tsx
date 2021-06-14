29e721b07b90a89e98e91733d6f67156c4a5fc42
Adrian Busse <git@adrianmxb.com>
2021-06-14 09:06:50 +0200

    remove unused imports

--- src/frontend/src/home/components/CryptosListComponent.tsx
+++ src/frontend/src/home/components/CryptosListComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c211237b9bd8aa686328589c54ffa5018c8b9ea37beea78aa6412e1ff6336446
-size 3718
+oid sha256:ae5423a585a0edce2a7d6ba23093aaff4c4daa7dccc48df892f278cb7e6b0170
+size 3552
--- src/frontend/src/home/components/NewsComponent.tsx
+++ src/frontend/src/home/components/NewsComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a3eaaeac989dc5e37f8af060dbf4eddb06341535e8b3ad7b415c1e535833043
-size 2404
+oid sha256:ddb705bfd6af87903b4602e19fd0c566d31ed281ee4d651ec34596846863bc89
+size 2325
