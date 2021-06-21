7d192ee690dc23ad9f2435e1b64cc1945f08ecf9
Diimsch <57183037+Diimsch@users.noreply.github.com>
2021-06-21 15:29:07 +0200

    ReOrder TradingView

--- src/frontend/src/trade/pages/Trade.tsx
+++ src/frontend/src/trade/pages/Trade.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:601004669ae133e6ef18228ae38f659a1baa18017c0910b536870b23c926906d
-size 3437
+oid sha256:ec0961fb0e896cadfc37598a9bdd1254fd866979c3ca29bb470c54ca08171620
+size 3764
