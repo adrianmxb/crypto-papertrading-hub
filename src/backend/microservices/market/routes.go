b3b50c880225b7c798041ef888d13c1266f421c0
Nico <nico24sg@gmail.com>
2021-06-21 11:34:40 +0200

    ticker should not be a get

--- src/backend/microservices/market/routes.go
+++ src/backend/microservices/market/routes.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e518cefa42bb8117ab3807d5a052e8ddefee1606691779d190bba8e8cddae80a
-size 378
+oid sha256:b2036506cbd4ea2da8656bba842a13ea68a59e3df3c3759a9394ae179daa01b9
+size 363
