c61d1ccb169545ceb255db508bb377a3e7d87741
Diimsch <57183037+Diimsch@users.noreply.github.com>
2021-06-21 09:51:27 +0200

    remove console.log

--- src/frontend/src/trade/components/OrderComponent.tsx
+++ src/frontend/src/trade/components/OrderComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:524195c5bfc80597eba996f29f486ae1a4054c8136437423ff8210ee702c59bc
-size 5663
+oid sha256:0d832bb932e3c08055b9c10f599e7eac8a09c30f52bd3ea2aa38f0f33d5425e4
+size 5602
