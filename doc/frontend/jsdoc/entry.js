fbab19a19e38d9b87747df54fa7f6cebceea517e
Adrian Busse <git@adrianmxb.com>
2021-06-24 18:00:11 +0200

    add frontend docs to /doc folder

---
+++ doc/frontend/jsdoc/entry.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7b60092b20599160622b1fda8058810d58bcc31e571672f6700ee1aca0fd66cb
+size 1977
---
+++ doc/frontend/jsdoc/scripts/linenumber.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6cf4e2a06ed5b87f422a0827075415a77715ccdc5454b8273fe327aafa15f72b
+size 675
