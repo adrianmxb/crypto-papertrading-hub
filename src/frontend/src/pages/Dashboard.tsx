6d06492051f41d8de8506c3aa01eea9310734e3e
Adrian Busse <git@adrianmxb.com>
2021-06-24 14:38:44 +0200

    fix path issue

--- src/frontend/src/pages/Dashboard.tsx
+++ src/frontend/src/pages/Dashboard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fac286f05159d81ee79deb9fba48403076ea1e58909aa89a98cde38c21acf27
-size 1684
+oid sha256:fff193f88f479599ba4a85b1f496eb23be108eb0305c0726435f732a8485ce2e
+size 1670
