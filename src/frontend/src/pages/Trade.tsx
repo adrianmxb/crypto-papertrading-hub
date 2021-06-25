f81e81b98ebb10437b034b51246760e1d5b645b8
Diimsch <57183037+Diimsch@users.noreply.github.com>
2021-06-25 22:17:45 +0200

    fix

--- src/frontend/src/pages/Trade.tsx
+++ src/frontend/src/pages/Trade.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:790bce9cd1da7853fb669a1d47c276ad015c0ab12560b6e6514ba2bfc258cdbf
-size 3860
+oid sha256:da841fa4bf811d7d8461871381091803d49c6c331cbae8a6149439a3f8a51745
+size 3912
