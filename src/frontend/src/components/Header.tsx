673c7f029b6dc8c69c664d89c5e9d004e5d53f14
Adrian Busse <git@adrianmxb.com>
2021-06-25 21:50:45 +0200

    fix media query used for mobile detection

--- src/frontend/src/components/Header.tsx
+++ src/frontend/src/components/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0f04b94176b85638af0e25956540e9f53d0c3ead739586d592227ca0f07a682
-size 4269
+oid sha256:810f6aa630af08a393dd06bb067fcd4d2cd891eafcf480f34a36528c3b398be6
+size 4262
