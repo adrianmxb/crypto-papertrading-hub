f385955753b93ab0cfffa9d8df4dd8d0fd55023d
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-06-26 14:48:29 +0200

    Rebuilt images based on master and updated deployment tags

--- src/deployment/terraform/apps.tf
+++ src/deployment/terraform/apps.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b49cfb56af9c61d5aadbe81fcff3298481c788534f556c830922d6260281f49a
+oid sha256:6c8ec5639195f516e93b255c7c0bd45fc53da9e737f7f1bcdf7cf9d2695fc20c
 size 2782
