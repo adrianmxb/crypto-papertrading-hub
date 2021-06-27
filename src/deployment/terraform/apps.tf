50cc68e82d8cb63ce7b0a4aeca6675263ccd4de6
Jannis Assenheimer <jannis.assenheimer@gmail.com>
2021-06-27 21:24:25 +0200

    Updating web-app deploy tag

--- src/deployment/terraform/apps.tf
+++ src/deployment/terraform/apps.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c8ec5639195f516e93b255c7c0bd45fc53da9e737f7f1bcdf7cf9d2695fc20c
+oid sha256:f3d738a9b33da63e2f66b255c9a774028f55641c91408d57e3424a32d3c4ae65
 size 2782
