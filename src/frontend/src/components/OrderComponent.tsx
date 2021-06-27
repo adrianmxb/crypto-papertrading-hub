312f625f08045c73781140a3c681e0a79d75ceb6
Adrian Busse <git@adrianmxb.com>
2021-06-27 22:11:22 +0200

    fix floating point issue in order component

--- src/frontend/src/components/OrderComponent.tsx
+++ src/frontend/src/components/OrderComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a2190d2fc4b67450406f898d9f7c5cd8b998a07e9a177469fe88eab4bde74db
-size 6193
+oid sha256:e5cff6044a6d2e59364b1f4d5722c52bb4f49dfda03a6a06a513f8b6aa403ac0
+size 6241
