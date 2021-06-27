685dbebad3a52a69a4e408e58655c43535a5f86b
Adrian Busse <git@adrianmxb.com>
2021-06-27 14:58:46 +0200

    fix dropdown selection overflow

--- src/frontend/src/components/OrderComponent.tsx
+++ src/frontend/src/components/OrderComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:076dee17ecc7211708876a7a5fbcb884fc52f1b478d9ee415ee4ed8f18b91505
-size 5749
+oid sha256:247fa4ca80f32a04a3beebd4ab4301724186bea66ca9755a7e417cd330ba439c
+size 5775
