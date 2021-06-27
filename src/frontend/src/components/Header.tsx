d993e49d1291952f9dbf34e25267e7399ac5a6eb
Adrian Busse <git@adrianmxb.com>
2021-06-27 11:28:10 +0200

    remove unused code

--- src/frontend/src/components/Header.tsx
+++ src/frontend/src/components/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:810f6aa630af08a393dd06bb067fcd4d2cd891eafcf480f34a36528c3b398be6
-size 4262
+oid sha256:b5349751668b90dffa7d70857984558b6b1bb69db6224bb2e6ea467123083b13
+size 2516
