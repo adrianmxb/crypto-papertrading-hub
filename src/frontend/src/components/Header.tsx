a2b97d9affc434f0f473a3fa9d3329ca4f0314c0
Adrian Busse <git@adrianmxb.com>
2021-06-27 16:02:35 +0200

    fix authentication issue in header component

--- src/frontend/src/components/Header.tsx
+++ src/frontend/src/components/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef69b55b210bade0104e0013775037df5b39bfcaeb59046d2ec46c2e894bf276
-size 2737
+oid sha256:ab010d6bc2432ec630b9b338756ed0c279a5cfb08340d1922ac200db9d04b5bb
+size 2738
