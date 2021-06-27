7f43b72ca89978be5ae94664bbdcacdc977dac43
Adrian Busse <git@adrianmxb.com>
2021-06-27 16:08:51 +0200

    regenerate docs

--- doc/frontend/build/entry.js
+++ doc/frontend/build/entry.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18e05c9a5981e9b4b6e5461601698fea7f7a6d5fa5a0eb347fdeee8c17fe6e34
-size 2645731
+oid sha256:63a3ec13f1a7d29fcd1fd64ef020cf1f0b6443f00aa4238bc75892fd7bad8e6a
+size 2645894
--- src/frontend/src/components/Header.tsx
+++ src/frontend/src/components/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab010d6bc2432ec630b9b338756ed0c279a5cfb08340d1922ac200db9d04b5bb
-size 2738
+oid sha256:50b44b6ede66b01e7ee4784f8a0f0e9db1c863b922f913f605b4d7a72ff5e658
+size 2694
