b9b006fec628a4691543e1eab258cd1cef44d527
Adrian Busse <git@adrianmxb.com>
2021-06-27 22:02:13 +0200

    add reconnect option for websocket and optimize websocket handling in general

--- src/frontend/src/pages/Main.tsx
+++ src/frontend/src/pages/Main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c7e5257d69048785cc5c04b0daa3392c995ccfae7bf98b42f274a2069b929bd
-size 3837
+oid sha256:204ba680e16d9c74a9cab75c40b350d0cb98d0b416a38a682f6dddb595c3d421
+size 4258
