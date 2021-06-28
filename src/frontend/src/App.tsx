d928a5ce614827637e8faf571dce2ab2fe3c1532
Adrian Busse <git@adrianmxb.com>
2021-06-28 09:17:38 +0200

    fix login issue

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83fa8349b4c2c40a880acea2383741d539de4983cc79eb098684c6b9526e4460
-size 1403
+oid sha256:38d38ba4b2bf0ac53acc24b575e82eb46ce543245b3db76d30c51d4f3d054414
+size 1548
