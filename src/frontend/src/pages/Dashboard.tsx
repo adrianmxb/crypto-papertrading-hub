90b3688e78f7554cc7acdee080b008fc7fe65c06
Adrian Busse <git@adrianmxb.com>
2021-06-28 08:42:38 +0200

    add auto focus on login input

--- src/frontend/src/pages/Dashboard.tsx
+++ src/frontend/src/pages/Dashboard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb2fceb50092dfd7b46df08bb8d48c9ab4a350792cc47fa445e88e26371a4bd2
-size 1936
+oid sha256:f0410e5c93124f07969419164121fad791185b07a2ac02115ab52cc9dd04c324
+size 1907
--- src/frontend/src/pages/Login.tsx
+++ src/frontend/src/pages/Login.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68c7e079afddf8bf0f56e1b7e6b040abf040002dfb7e705abd69e68f896eb91b
-size 4987
+oid sha256:0e4f3126ed24d994215f4293e93cf5bc4c4ddf06c5f2057d04e3dffa627ff13d
+size 5011
